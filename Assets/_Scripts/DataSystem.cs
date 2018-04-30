a2fca2100ea66b1834622988e465bbe14030dc5d
jfcarocota <jfcarocota@gmail.com>
2018-04-30 13:05:49 -0700

    clase 30/04/2018 parte 1

---
+++ Assets/_Scripts/DataSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUtils
+{
+    namespace DataSystem
+    {
+        public static class DataSystem
+        {
+            public static void SaveData()
+            {
+
+            }
+
+            public static void NewGame()
+            {
+
+            }
+
+            public static void LoadGame()
+            {
+
+            }
+
+            public static void PopulateDataBlocks()
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/GameData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class GameData
+{
+    [SerializeField]
+    int level;
+    [SerializeField]
+    int magicPower;
+    [SerializeField]
+    int mana;
+    [SerializeField]
+    int attackPower;
+    [SerializeField]
+    int deffense;
+    [SerializeField]
+    int magicDeffense;
+
+    public GameData(int level, int magicPower, int mana, int attackPower, int deffense, int magicDeffense)
+    {
+        this.level = level;
+        this.magicPower = magicPower;
+        this.mana = mana;
+        this.attackPower = attackPower;
+        this.deffense = deffense;
+        this.magicPower = magicPower;
+    }
+
+
+}
